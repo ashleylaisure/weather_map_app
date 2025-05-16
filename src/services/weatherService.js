7909ab0e360dc59347be611237b8fd45c7332a96
Ashley Laisure <ashley.laisure@gmail.com>
2025-05-16 14:56:24 -0400

    API key updates

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,24 +1,14 @@
-// const API_KEY = '&units=imperial&appid=247662426988f015cc27f4f779500a05'
-const API_KEY = import.meta.env.VITE_API_KEY;
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+// const API_KEY = import.meta.env.VITE_API_KEY;
+// const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+
+const BASE_URL = `http://localhost:3000/weather/`;
 
 const show = async (city) => {
     try{
-        if (!isNaN(city) && !isNaN(parseFloat(city))) {
-            const queryString = `zip=${city}`
-            const res = await fetch(BASE_URL + queryString + API_KEY)
-            const data = await res.json()
-            // console.log("Data:", data)
-            return data;
-
-        } else {
-            const queryString = `q=${city}`;
-            const res = await fetch(BASE_URL + queryString + API_KEY)
-            const data = await res.json()
-            // console.log("Data:", data)
-            return data;
-        }
-        
+        const res = await fetch(BASE_URL + city)
+        const data = await res.json()
+        console.log("Data:", data)
+        return data;
         
     } catch (err) {
         console.log(err)
